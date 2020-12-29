5ca0e5d5cb01904eb10b659cbdd197a8af8df03e
mohamad7islam <76557504+mohamad7islam@users.noreply.github.com>
2020-12-29 04:03:04 +0300

    finished CW-6 completely and added the bonus (input validation )

--- swiftiu-sc-6/swiftiu-sc-6/ContentView.swift
+++ swiftiu-sc-6/swiftiu-sc-6/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct BMI: View {
     @State var h : String = ""
     @State var w : String = ""
+    @Binding var BMI_val : String
+    @Binding var BMI_status : String
+    @State private var showsAlert = false
     var body: some View {
         VStack(){
         Text("مرحبا بك بحاسبة BMI")
@@ -25,33 +28,94 @@ struct BMI: View {
             .multilineTextAlignment(.trailing)
             .textFieldStyle(RoundedBorderTextFieldStyle())
             .padding()
+            
             Button(action: {
-                var BMI_val = Int(w)! / ( Int(h)! * Int(h)! )
-            }, label: {
+              //  let hd = Double(h)!
+             //   let wd = Double(w)!
+                if  Double(h)! < 0.5 || Double(h)! > 2.5 || Double(w)! < 30 || Double(w)! > 350 {
+                    showsAlert = true}
+                else{
+                showsAlert = false
+                let B = Double(w)! / ( Double(h)! * Double(h)! )
+
+                if B > 25 {
+                    BMI_status = "سمين"
+                } else if B > 20 {
+                    BMI_status = "جيد"
+                } else {
+                    BMI_status = "ضعيف"
+                }
+                BMI_val = String(format: "%.1f", B)
+                    }
+                }
+                ,label: {
                 Text("احسب")
                     .padding()
                     .foregroundColor(.white)
                     .background(Color.blue)
                     .clipShape(Capsule())
-                
-                    
-                    
             })
-        
-        }
-        
-}
-}
-    struct ContentView: View {
-        var body: some View {
-            Text("HI")
-        
-    }
-    }
+            Text("BMI")
+                .font(.title)
+                .bold()
+                .padding()
+                .alert(isPresented: $showsAlert) {
+                                Alert(
+                                    title: Text("Freez in ur place 🔫"),
+                                    message: Text("make sure you entered the right values :("),
+                                    dismissButton: .default(Text("OK , I got it"))
+                                )
+                            }
+            Text("\(BMI_val)")
+                .font(.title)
+                .bold()
+                .padding()
+            Text("Status")
+                .font(.title)
+                .bold()
+                .padding()
+            Text("\(BMI_status)")
+                .font(.title)
+                .bold()
+                .padding()
+            }
 
+}
+    
+}
+
+    struct ContentView: View {
+        @State var BMI_val = ""
+        @State var BMI_status = ""
+        var body: some View {
+            NavigationView{
+            VStack(){
+            Text("BMI : \(BMI_val) ")
+                .font(.title)
+                .bold()
+                .padding()
+            Text("Status : \(BMI_status) ")
+                .font(.title)
+                .bold()
+                .padding()
+                NavigationLink(
+                    destination: BMI(BMI_val: $BMI_val, BMI_status: $BMI_status),
+                    label: {
+                        Text("Go to calculator")
+                            .padding()
+                            .foregroundColor(.white)
+                            .background(Color.blue)
+                            .clipShape(Capsule())
+                    })
+                
+                
+    }
+    }
+    }
+    }
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        BMI()
+        ContentView()
             .previewDevice("iPhone 11")
     }
 }
